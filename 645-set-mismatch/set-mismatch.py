ee922ab6ef6ed3a4bafb361b5e87ad3e55d73ae0
Divya Gupta <divya9upt@gmail.com>
2026-08-19 19:28:26 +0530

    Time: 4 ms (93.19%) | Memory: 20.2 MB (98.54%) - LeetSync

---
+++ 645-set-mismatch/set-mismatch.py
@@ -0,0 +1,19 @@
+class Solution:
+    def findErrorNums(self, nums: list[int]) -> list[int]:
+        n = len(nums)
+        actual_sum = sum(nums)
+        actual_sq_sum = sum(x * x for x in nums)
+        
+        expected_sum = n * (n + 1) // 2
+        expected_sq_sum = n * (n + 1) * (2 * n + 1) // 6
+        
+        diff = actual_sum - expected_sum
+    
+        sq_diff = actual_sq_sum - expected_sq_sum
+        
+        total = sq_diff // diff
+        
+        duplicate = (diff + total) // 2
+        missing = total - duplicate
+        
+        return [duplicate, missing]
