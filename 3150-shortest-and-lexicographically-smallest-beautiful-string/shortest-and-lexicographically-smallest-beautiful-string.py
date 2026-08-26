a80d73db24033280adf948b8ed2958d9a45b4ca2
Divya Gupta <divya9upt@gmail.com>
2026-08-26 22:30:19 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (30.63%) - LeetSync

--- 3150-shortest-and-lexicographically-smallest-beautiful-string/shortest-and-lexicographically-smallest-beautiful-string.py
+++ 3150-shortest-and-lexicographically-smallest-beautiful-string/shortest-and-lexicographically-smallest-beautiful-string.py
@@ -7,10 +7,7 @@ class Solution:
         for right in range(len(s)):
             if s[right] == '1':
                 count += 1
-
-            # Window mein exactly k ones
             while count == k:
-                # Left ke unnecessary zero hatao
                 while left <= right and s[left] == '0':
                     left += 1
 
@@ -22,8 +19,6 @@ class Solution:
                     ans = current
                 elif len(current) == len(ans) and current < ans:
                     ans = current
-
-                # Next possible window ke liye left wala 1 hatao
                 if s[left] == '1':
                     count -= 1
 
