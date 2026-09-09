e0091d6a8a480dff2690719116fb5f7c8fe45af3
Divya Gupta <divya9upt@gmail.com>
2026-09-09 19:49:52 +0530

    as

--- 10-regular-expression-matching/regular-expression-matching.py
+++ 10-regular-expression-matching/regular-expression-matching.py
@@ -10,7 +10,6 @@ class Solution:
             first_match = i < len(s) and p[j] in (s[i], '.')
 
             if j + 1 < len(p) and p[j + 1] == '*':
-                # zero occurrences OR one-plus occurrences
                 return dp(i, j + 2) or (first_match and dp(i + 1, j))
             else:
                 return first_match and dp(i + 1, j + 1)
